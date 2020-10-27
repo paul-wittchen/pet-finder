79aa007f93b6a8a43b35adc2dacc7646ccc3fee1
Paul Wittchen <wittchenpaul@gmail.com>
2020-10-27 13:09:00 +0100

    mongodb setup

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-http.createServer(app).listen(8000, function() {
-    console.log('Server listening on port 8000');
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB running");
+})
+
+const port = process.env.PORT || 8000
+
+http.createServer(app).listen(port, () => {
+    console.log('Server listening on port ' + port);
 });
