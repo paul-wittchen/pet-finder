1c1262d205879d8e225732e49780a99e12130f8a
Paul Wittchen <wittchenpaul@gmail.com>
2020-10-30 17:12:29 +0100

    added signup styling

--- client/src/components/user/signup.component.js
+++ client/src/components/user/signup.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import '../../styles/signup.scss';
+import dog from '../../images/signup_pic.png';
 
 export default class Signup extends Component {
     constructor() {
@@ -37,50 +39,67 @@ export default class Signup extends Component {
 
     render() {
         return(
-            <Form onSubmit={this.onSubmit}>
-                <Form.Group controlId="firstname">
-                    <Form.Label>Firstname</Form.Label>
-                    <Form.Control 
-                        type="text"
-                        name='firstname'
-                        value={this.state.firstname}
-                        onChange={this.onChange}
-                    />
-                </Form.Group>
-                <Form.Group controlId="lastname">
-                    <Form.Label>Lastname</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        name='lastname'
-                        value={this.state.lastname}
-                        onChange={this.onChange}
-                    />
-                </Form.Group>
-                <Form.Group controlId="email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                        type="email"
-                        name='email'
-                        value={this.state.email}
-                        onChange={this.onChange}
-                    />
-                </Form.Group>
-                <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        type="password"
-                        name='password'
-                        value={this.state.password}
-                        onChange={this.onChange}
-                    />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+            <Row className='main__container__signup text-center'>
+                <Col className='signup__container__left'>
+                    <p className='signup__header'>Create Account</p>
+                    <p className='signup__subheader'>Already have an account? <a href="/login">Sign in</a></p>
+                    <Form onSubmit={this.onSubmit}>
+                        <Row>
+                            <Col>
+                            <Form.Group controlId="firstname">
+                            <Form.Label>Firstname</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                name='firstname'
+                                value={this.state.firstname}
+                                onChange={this.onChange}
+                            />
+                        </Form.Group>
+                            </Col>
+                            <Col>
+                            <Form.Group controlId="lastname">
+                            <Form.Label>Lastname</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                name='lastname'
+                                value={this.state.lastname}
+                                onChange={this.onChange}
+                            />
+                        </Form.Group>
+                            </Col>
+                        </Row>
+                        
+                        
+                        <Form.Group controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control 
+                                type="email"
+                                name='email'
+                                value={this.state.email}
+                                onChange={this.onChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                                type="password"
+                                name='password'
+                                value={this.state.password}
+                                onChange={this.onChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="I agree to the Terms of Service and Privacy Policy" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Sign up
+                        </Button>
+                    </Form>
+                </Col>
+                <Col className='signup__container__right'>
+                    <img src={dog} alt="signup_picture"/>
+                </Col>
+            </Row>
         )
     }
 }
