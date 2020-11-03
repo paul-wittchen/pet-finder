3477cfb68182ef60dcc48d03b8a6d32b827c0bba
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-03 14:35:46 +0100

    main details-page styling

--- client/src/components/pet/petDetails.component.js
+++ client/src/components/pet/petDetails.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import Moment from 'react-moment';
+import '../../styles/petDetails.scss';
 
 export default class PetDetails extends Component {
     constructor(props) {
@@ -20,8 +23,39 @@ export default class PetDetails extends Component {
     render() {
         console.log(this.state.petData.petName);
         return(
-            <div>
-                <h1>{this.state.petData.petName}</h1>
+            <div className='pet__details__container'>
+                <Row className='pet__details__row'>
+                    <Col className='pet__details__left'>
+                        <img src="https://source.unsplash.com/user/adventure_yuki/640x400" alt=""/>
+                    </Col>
+                    <Col className='pet__details__right'>
+                        <p className='pet__details__name'>{this.state.petData.petName}</p>
+                        <p className='pet__details__desc'>{this.state.petData.description}</p>
+                        <Row className='pet__details__info__row'>
+                            <Col>
+                                <p className='pet__details__row__desc'>Kind:</p>
+                                <p className='pet__details__infos'>{this.state.petData.petKind}</p>
+                            </Col>
+                            <Col>
+                                <p className='pet__details__row__desc'>Reward:</p>
+                                { this.state.petData.reward === '' ? (
+                                    <p className='pet__details__infos'> - </p>
+                                ) : (
+                                    <p className='pet__details__infos'>{this.state.petData.reward}</p>
+                                )}
+                            </Col>
+                            <Col>
+                                <p className='pet__details__row__desc'>Where:</p>
+                                <p className='pet__details__infos'>{this.state.petData.location}</p>
+                            </Col>
+                            <Col>
+                                <p className='pet__details__row__desc'>Lost:</p>
+                                <p className='pet__details__infos'><Moment fromNow>{this.state.petData.createdAt}</Moment></p>
+                            </Col>
+                        </Row>
+                        
+                    </Col>
+                </Row>
             </div>
         )
     }
