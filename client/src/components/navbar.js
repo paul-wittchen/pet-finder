6e4b11e55ea005c0c8f64ff96ac130c6bc1e6c40
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-10 12:46:10 +0100

    navbar styling done

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -22,26 +22,28 @@ export default class Navigation extends Component {
     render() {
         if (!this.state.loading) {
             return(
-                <Navbar className='navbar__main' variant="light">
-                    <Navbar.Brand href="/">PetFinder</Navbar.Brand>
-                    <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/pets-list">All-pets</Nav.Link>
-                    {this.state.loggedIn ? (
-                        <Nav.Link href="/lost-pet">Lost-a-pet</Nav.Link>
-                    ) : (
-                        <Nav.Link href="/signup">Lost-a-pet</Nav.Link>
-                    )}
-                    {this.state.loggedIn ? (
-                        <Nav.Link href='/profile'>Profile</Nav.Link>
-                    ) : (
-                        <>
-                        <Nav.Link href="/signup">Signup</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link> 
-                        </>  
-                    )}       
-                    </Nav>
-                </Navbar>
+                <div className='navbar__container justify-content-center text-center'>
+                    <Navbar className='navbar__main'>
+                        <Nav className="mr-auto">
+                        <Navbar.Brand href="/">PetFinder</Navbar.Brand>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/pets-list">All-pets</Nav.Link>
+                        {this.state.loggedIn ? (
+                            <Nav.Link href="/lost-pet">Lost-a-pet</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/signup">Lost-a-pet</Nav.Link>
+                        )}
+                        {this.state.loggedIn ? (
+                            <Nav.Link href='/profile'>Profile</Nav.Link>
+                        ) : (
+                            <>
+                            <Nav.Link href="/signup">Signup</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link> 
+                            </>  
+                        )}       
+                        </Nav>
+                    </Navbar>
+                </div>
             )
         } else {
             return <></>
