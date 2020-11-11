5f254e1e91fa6051c21506de89529d12050c37bd
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-11 16:30:35 +0100

    new deployment

--- client/src/utility.js
+++ client/src/utility.js
@@ -1,7 +1,3 @@
 const backendDomain = process.env.NODE_ENV === 'production' ? 'https://pet--finder.herokuapp.com' : 'http://localhost:8000'
 
-const exportObject = {
-    backendDomain
-}
-
-export default exportObject
+export default backendDomain
