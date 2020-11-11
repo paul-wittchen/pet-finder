d250dfbc314a5b1ea1f15ad59c826108dd98baf2
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-11 15:03:26 +0100

    added reading

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Build
 app.get(express.static(path.join(__dirname, 'build')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(api)
 
