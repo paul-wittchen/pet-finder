40cfc045346bb0882fcb0cc6d2d785295f2f0462
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-11 16:55:05 +0100

    new deployment + email regex

--- client/src/components/user/signup.component.js
+++ client/src/components/user/signup.component.js
@@ -40,7 +40,7 @@ export default class Signup extends Component {
     onSubmit = (event) => {
         event.preventDefault();
 
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
         if (this.state.firstname === '') {
             this.setState({ firstnameError: 'Please enter your firstname'})
