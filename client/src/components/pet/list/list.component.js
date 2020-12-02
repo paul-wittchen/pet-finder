207cf2fca10722bb7af793aaed104ba94996e13a
Paul Wittchen <wittchenpaul@gmail.com>
2020-12-02 14:01:14 +0100

    added new build

--- client/src/components/pet/list/list.component.js
+++ client/src/components/pet/list/list.component.js
@@ -17,38 +17,37 @@ export default class ListPets extends Component {
     componentDidMount() {
         axios
             .post(`${backendDomain}/pets-list`)
-            .then((res) => {
-                this.setState({
-                    pets: res.data.pets
-                })
-            })
+            .then((res) => { this.setState({ pets: res.data.pets }) })
             .catch((error) => console.log(error))
     }
 
     render() {
-        console.log(this.state);
         return(
             <div className='pets__list__container'>
                 <p className='pets__list__header'>The "runaways"</p>
-                <Row className='justify-content-center'>
-                    {this.state.pets.map(pet => 
-                        <Col lg={4} md={6} xs={12}>
-                            <PetCard
-                                uuid={pet.uuid}
-                                imageURL={pet.image}
-                                petName={pet.petName}
-                                petKind={pet.petKind}
-                                description={pet.description}
-                                location={pet.location}
-                                reward={pet.reward}
-                                createdAt={pet.createdAt}
-                                phone={pet.phone}
-                                mail={pet.mail}
-                                key={pet.createdAt}
-                            />
-                        </Col>
-                    )}
-                </Row>
+                { this.state.pets && this.state.pets.length < 1 ? (
+                    <p className='pets__list__empty'>There are no open search calls</p>
+                ) : (
+                    <Row className='justify-content-center'>
+                        {this.state.pets.map(pet => 
+                            <Col lg={4} md={6} xs={12}>
+                                <PetCard
+                                    uuid={pet.uuid}
+                                    imageURL={pet.image}
+                                    petName={pet.petName}
+                                    petKind={pet.petKind}
+                                    description={pet.description}
+                                    location={pet.location}
+                                    reward={pet.reward}
+                                    createdAt={pet.createdAt}
+                                    phone={pet.phone}
+                                    mail={pet.mail}
+                                    key={pet.createdAt}
+                                />
+                            </Col>
+                        )}
+                    </Row>
+                )}
             </div>
         )
     }
