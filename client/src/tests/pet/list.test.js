1e9f13b4daed31a403516d8a80d969abd60c0734
Paul Wittchen <wittchenpaul@gmail.com>
2020-12-22 10:28:05 +0100

    added new test

--- client/src/tests/pet/list.test.js
+++ client/src/tests/pet/list.test.js
@@ -4,10 +4,16 @@ import ListPets from '../../components/pet/list/list.component';
 import Adapter from 'enzyme-adapter-react-16';
 
 describe('List Component', () => {
-    test('renders', () => {
-        const component = shallow(<ListPets/>);
-        expect(component.exists()).toBe(true)
+    it('renders as expected', () => {
+        const wrapper = shallow(<ListPets/>);
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('Div is not longer then 80 lines', () => {
+        const wrapper = shallow(<ListPets/>);
+        expect(wrapper.exists('.pets__list__empty')).toBe(true);
     })
 })
+
 
 Enzyme.configure({ adapter: new Adapter() });
